10e81308cc66d0dc3427f00d7b6d54ee8bb3c153
korsmann <jkorsvik@gmail.com>
2016-12-22 12:07:11 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# new
-tell
+# Telldus Alexa Home Skill 
+Integrates [Telldus Live](http://live.telldus.com/) with Alexa as a Smart Home Skill. 
+
+## Setup
+1. In the `Global.groovy` file, update it with the needed keys. See [Telldus documentation](https://api.telldus.com/keys/index) for more information 
+1. Run gradle/gradlew uberjar to build a deployable package of alexa-home-telldus. This will generate a `alexa-home-telldus-1.0.jar` file that should be uploaded to AWS Lamda
+
+1. Create an Alexa skill and Lambda Function by following [these instructions](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/steps-to-create-a-smart-home-skill) (with the modifications noted below).
+    * The name of the Alexa: Telldus-Alexa
+    * The name of the Lambda function: Telldus-Alexa-Service
+	* Select Runtime: Java8
+    * Select `lambda_basic_execution` for the Lambda role
+	* For "Handler", enter "Telldus::handler"
+    * Select "Upload a .ZIP or JAR file" for "Code entry type", and upload `alexa-home-telldus-1.0.jar` that you created in the previous step. 
+    * Add Trigger: "Alexa Smart Home"
+  
+## Usage
+After completing setup of alexa-home-telldus, tell Alexa: "Alexa, discover my devices".
+
+Example of commands for Alexa: 
+- Alexa, turn off the `<device name>`
+- Alexa, turn on the `<device name>`
+- Alexa, set `<name>` to `<number>` percent
+- Alexa, increase `<device name>` by `<number>` percent
+- Alexa, decrease `<device name>` by `<number>` percent
+
+
+History of commands to Alexa under `Menu / Settings / History` in the [web](http://echo.amazon.com/#settings/dialogs) or mobile app.
+To view or remove devices that Alexa knows about, you can go to `Menu / Smart Home` in the [web](http://echo.amazon.com/#smart-home) or mobile app.
---
+++ src/main/groovy/Telldus.groovy
@@ -0,0 +1,231 @@
+import com.amazonaws.services.lambda.runtime.Context; 
+import static java.util.UUID.randomUUID
+import org.scribe.builder.*
+import org.scribe.builder.api.*
+import org.scribe.model.*
+import org.scribe.oauth.*
+import groovy.json.JsonSlurper
+import groovy.json.*
+
+public class Telldus{
+		
+	public static void main(String[] args) {
+		def telldus = new Telldus()
+		telldus.handler([header:[messageId:'6d6d6e14-8aee-473e-8c24-0d31ff9c17a2', name:'TurnOnRequest', namespace:'Alexa.ConnectedHome.Control', payloadVersion:'2'], payload:[accessToken:'Atza|5A', appliance:[applianceId:1068213, additionalApplianceDetails:[extraDetail3:'but they should only be used for reference purposes.', extraDetail4:'This is not a suitable place to maintain current device state', extraDetail1:'optionalDetailForSkillAdapterToReferenceThisDevice, extraDetail2:There can be multiple entries']]]], null)
+		
+	}
+	def handler(def event, Context context){
+		println "INFO: " + event 
+		if (event.header.namespace == 'Alexa.ConnectedHome.Discovery') {
+			println "INFO: Alexa.ConnectedHome.Discovery - Sending response" 
+			return getDevicesRequest()
+		} else if (event.header.namespace == 'Alexa.ConnectedHome.Control') {
+			println "INFO: Alexa.ConnectedHome.Control"
+			def deviceId = event.payload.appliance.applianceId
+			def messageId = event.header.messageId
+			def headerName = event.header.name
+			
+				switch(headerName){
+                    case "TurnOnRequest": 
+                        println "INFO: Handle TurnOnRequest" 
+						return doRequest(deviceId, "turnOn", messageId, "TurnOnConfirmation", null, null)
+                        break 
+                    case "TurnOffRequest": 
+                        println "INFO: Handle TurnOffRequest"
+                        return doRequest(deviceId, "turnOff", messageId, "TurnOffConfirmation", null, null)
+						break
+                    case "HealthCheckRequest": 
+                        println "INFO: Handle HealthCheckRequest"
+						return doHealthCheckRequest()
+                        break 
+                    case "SetPercentageRequest":
+						def percentageState = event.payload.percentageState.value
+                        println "INFO: Handle SetPercentageRequest"
+						return doRequest(deviceId, "dim", messageId, "SetPercentageConfirmation", percentageState, null)
+                        break
+                    case "IncrementPercentageRequest":                         
+						def deltaPercentage = event.payload.deltaPercentage.value
+						println "INFO: Handle IncrementPercentageRequest"
+						return doRequest(deviceId, "dim", messageId, "IncrementPercentageConfirmation", deltaPercentage, 'up')
+                        break 
+                    case "DecrementPercentageRequest":
+                        def deltaPercentage = event.payload.deltaPercentage.value
+						println "INFO: Handle DecrementPercentageRequest"
+						return doRequest(deviceId, "dim", messageId, "DecrementPercentageConfirmation", deltaPercentage, 'down')
+                        break 
+                }		
+		}
+
+	}
+	
+	def messageId () {
+		return randomUUID() as String
+	}
+	
+	def doHealthCheckRequest() {
+		def reply = [:]
+		reply['header'] = ["namespace": 'Alexa.ConnectedHome.System', "name": HealthCheckResponse, "messageId": messageId(), "payloadVersion": '2']
+		
+		reply['payload'] = ["description": "The system is currently healthy","isHealthy": true]
+		return reply 
+	}
+	
+	def makeError(def name, def namespace, def payload) {
+		def error = [:]
+		error['header'] = ["namespace": namespace, "name": name, "messageId": messageId(), "payloadVersion": '2']
+		
+		error['payload'] = [:]
+		
+		return error 
+	}
+	def getCurrentState(def id) {
+		def params = [:]
+		params.put("id", id)
+		OAuthRequest request = createAndSignRequest("device/info", params)
+		Response response = request.send()
+		JsonSlurper jsonSlurper = new JsonSlurper()
+        def json = new JsonSlurper().parseText(response.getBody())
+		
+		return json.statevalue
+	}
+	
+	def doRequest(def id, def type, def messageId, def returnName, def level, def dimType) {
+		def params = [:]
+		def reply = [:]
+		
+		params.put("id", id)
+		if (level) {
+			def valueLevel = level.toInteger() * 2.55
+			def currentValue= ''
+			if(dimType == 'up') {
+				currentValue = getCurrentState(id).toInteger() 
+				valueLevel = valueLevel + currentValue
+				 
+			} else if (dimType == 'down') {
+				currentValue = getCurrentState(id).toInteger() 
+				valueLevel = currentValue - valueLevel
+				
+			}
+			println "INFO: Dim value: " + valueLevel
+			if (valueLevel > 255) {
+				valueLevel = 255
+			} else if (valueLevel < 0 ){
+				valueLevel = 0
+			}
+			params.put("level", valueLevel)
+		}
+		OAuthRequest request = createAndSignRequest("device/" + type, params)
+		
+		println "INFO: Sending request for id: " + id
+		Response response = request.send()
+		
+		JsonSlurper jsonSlurper = new JsonSlurper()
+        def json = new JsonSlurper().parseText(response.getBody())
+		
+		println "INFO: Telldus reply: " + json 
+		
+		if (json.status == 'success') {
+			reply['header'] = ["namespace": 'Alexa.ConnectedHome.Control', "name": returnName, "messageId": messageId, "payloadVersion": '2']
+		} else {
+			return makeError('TargetOfflineError','Alexa.ConnectedHome.Control')
+		}
+		
+		reply['payload'] = [:]
+		
+		println "INFO: Control reply " + reply 
+		return reply 
+	}
+	
+	def getDevicesRequest() { 
+	    def reply=[:]
+		println "INFO: getDevicesRequest"
+			
+		OAuthRequest request = createAndSignRequest("devices/list", ["supportedMethods":1023])
+		Response response = request.send()
+        def json = new JsonSlurper().parseText(response.getBody())
+		
+		reply['header'] = ["namespace": 'Alexa.ConnectedHome.Discovery', "name": 'DiscoverAppliancesResponse', "messageId": messageId(), "payloadVersion": '2']
+			
+		def getAllDevices = []
+		json.device.each{ telldus->
+			 def actions = ''
+
+			 if (telldus."methods"==51) {
+				actions = ["incrementPercentage", "decrementPercentage", "setPercentage", "turnOn", "turnOff"]
+			 } else {
+				actions = ["turnOn", "turnOff"]
+			 }
+			 
+			 getAllDevices.add(["applianceId":telldus."id",
+                    "manufacturerName":"Telldus",
+                    "modelName":"Unknown",
+                    "version":"1",
+                    "friendlyName":telldus."name",
+                    "friendlyDescription":"Telldus - " + telldus."name",
+                    "isReachable":true,
+                    "actions":actions,
+                    "additionalApplianceDetails":[
+						 "methods":telldus."methods"
+						]
+					])	
+			
+				
+		}
+		reply['payload'] = ["discoveredAppliances":getAllDevices]
+	
+		println "INFO: Device request reply: " + reply
+		return reply
+		
+	}
+	
+	def telldusUrl(String extension) {
+		return "http://api.telldus.com/json/" + extension
+	}
+
+	def OAuthRequest createAndSignRequest(String url, Map<String, String> parameters) {
+		println "INFO: createAndSignRequest url: " + url  + " Parameters: " + parameters
+		OAuthRequest request = createRequest(url, parameters)
+		OAuthService oService = createAuthService()
+		Token accessToken = createAccessToken()
+		
+		oService.signRequest(accessToken, request)
+		
+		return request
+	}
+
+	def Token createAccessToken() {
+		Global global = new Global(); 
+		String publicToken = global.getPublictoken()
+		String secretToken = global.getSecrettoken()
+
+		Token accessToken = new Token(publicToken, secretToken)
+		
+		return accessToken
+	}
+
+	def OAuthService createAuthService() {
+		Global global = new Global(); 
+		String publicKey = global.getPublickey()
+		String secretKey = global.getSecretkey()
+
+		OAuthService oService = new ServiceBuilder().provider(GoogleApi).apiKey(publicKey).apiSecret(secretKey).build()
+		return oService
+	}
+
+	def OAuthRequest createRequest(String url, Map<String, String> parameters) {
+		// Create, sign and send request.
+		OAuthRequest request = new OAuthRequest(Verb.GET, telldusUrl(url))
+
+		if (parameters) {
+			parameters.each {key, value ->
+				request.addQuerystringParameter(key, value.toString())
+			}	
+		}
+
+		return request
+	}
+	
+}
+
+
+
