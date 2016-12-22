64ccde81e570dde2508efec24eaa2f61bf68a763
korsmann <jkorsvik@gmail.com>
2016-12-22 12:08:55 +0100

    Update Global.groovy
    Lagt til keys

--- src/main/groovy/Global.groovy
+++ src/main/groovy/Global.groovy
@@ -1,6 +1,6 @@
 class Global {
-  static String publictoken = ''
-  static String secrettoken = ''
-  static String publickey =  ''
-  static String secretkey =  ''
+  static String publictoken = '52a8a4123d9a2ffdeb3a608187948f0f0585b85a1'
+  static String secrettoken = '495086804289aa44229f64d37bbf3a93'
+  static String publickey =  'FEHUVEW84RAFR5SP22RABURUPHAFRUNU'
+  static String secretkey =  'ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF'
 }
